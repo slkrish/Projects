0b46e5a2c2973d8230475405c37013863d9dc493
Saikrishna Peraveni <s.peraveni@zensar.com>
2022-07-11 10:22:13 +0530

    Final CodeBase

---
+++ CodeBaseFinalEvaluation/CodeBaseFinalEvaluation/Data/CodeBaseFinalEvaluationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CodeBaseFinalEvaluation.Models;
+
+namespace CodeBaseFinalEvaluation.Data
+{
+    public class CodeBaseFinalEvaluationContext : DbContext
+    {
+        public CodeBaseFinalEvaluationContext (DbContextOptions<CodeBaseFinalEvaluationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CodeBaseFinalEvaluation.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ CodeBaseFinalEvaluation/CodeBaseFinalEvaluation/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeBaseFinalEvaluation.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        [Required(ErrorMessage = "Please Enter First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Location { get; set; }
+
+    }
+}
